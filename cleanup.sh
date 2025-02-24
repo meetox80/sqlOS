d01018980757670221c27eea360cc3f9babbb3ed
lmq <72318911+meetox80@users.noreply.github.com>
2025-02-24 01:21:24 +0100

    fix: .gitignore & cleanup.sh .elf addition

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f \( -name "*.img" -o -name "*.o" -o -name "*.bin" -o -name "*.iso" \) -delete
+find . -type f \( -name "*.img" -o -name "*.o" -o -name "*.bin" -o -name "*.iso" -o -name "*.elf" \) -delete
