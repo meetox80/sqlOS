36a31127626eff01c9b1696b774742462afd866c
lmq <72318911+meetox80@users.noreply.github.com>
2025-02-24 02:16:44 +0100

    draw 8-bit colorspace

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,10 +6,31 @@ void _start()
 {
     Draw->Init();
     
-    for (uint32_t I = 0; I < _WIDTH * _HEIGHT; I++) 
+    for (uint32_t i = 0; i < _WIDTH * _HEIGHT; i++) 
     {
-        Draw->_FrameBuffer[I] = 0x0F;
+        Draw->_FrameBuffer[i] = 0x0F;
     }
+
+    const int CELL_SIZE = 10;
+    const int SPACING = 2;
+    const int COLS = 16; // (sqrt(256)
+
+    for (uint32_t i = 0; i < 256; i++) {
+        const int col = i % COLS;
+        const int row = i / COLS;
+        
+        const int x = col * (CELL_SIZE + SPACING);
+        const int y = row * (CELL_SIZE + SPACING);
+        
+        Draw->DrawRectangle(
+            x,
+            y,
+            CELL_SIZE,
+            CELL_SIZE,
+            (uint8_t)i
+        );
+    }
+
     
     while(1) {
         __asm__("hlt");
